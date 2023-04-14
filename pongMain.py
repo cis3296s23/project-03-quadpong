1d6b81e068b86c63267d3c32f028c5e2cd3ec092
domo9854 <81871640+domo9854@users.noreply.github.com>
2023-04-14 12:09:44 -0400

    fized issue when quiting the settings it ended the program, now goes back to main splash screen menu.

--- pongMain.py
+++ pongMain.py
@@ -1,29 +1,5 @@
+import splashscreenSETTINGS
+import splashscreen
+import pygame
 
-
-title = Tk()
-title.title("Quad Pong")
-label_1 = Label(title, text = "Welcome To Quad Pong")
-label_2 = Label(title, text = "RULES")
-label_1.pack()
-label_2.pack()
-
-wn = turtle.Screen()
-wn.title("Quad Pong")
-wn.bgcolor("grey")
-wn.setup(width=1000, height=800)
-wn.tracer(0)
-
-#Border
-bd = turtle.Turtle()
-bd.penup()
-bd.goto(-370,-270)
-bd.pendown()
-bd.pensize(5)
-bd.fd(740)
-bd.left(90)
-bd.fd(540)
-bd.left(90)
-bd.fd(740)
-bd.left(90)
-bd.fd(540)
-bd.hideturtle()
+splashscreen.splashscreen()
