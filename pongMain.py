70eb6169f5d81326deea7a5f068e8a42f9facd24
Rishi Duggal <72986170+RishiDuggal@users.noreply.github.com>
2023-04-21 02:21:48 -0400

    gamerunner now produces running game

--- pongMain.py
+++ pongMain.py
@@ -1,5 +1,9 @@
 import splashscreenSETTINGS
 import splashscreen
 import pygame
+from pong import gameRunner
+from gameObjs import ball, paddle
 
-splashscreen.splashscreen()
+#splashscreen.splashscreen() #uncomment for splash screen to work, otherwise change gamerunner obj to test
+
+game = gameRunner("twoplayer", 1, 1, 1)
