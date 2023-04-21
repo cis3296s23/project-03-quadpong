a9c8c021f871ab67c3efa00b1ddece73fbb7faf8
Rishi Duggal <72986170+RishiDuggal@users.noreply.github.com>
2023-04-21 02:29:47 -0400

    fixed fps issue by closing splashscreen

--- pongMain.py
+++ pongMain.py
@@ -4,6 +4,6 @@ import pygame
 from pong import gameRunner
 from gameObjs import ball, paddle
 
-#splashscreen.splashscreen() #uncomment for splash screen to work, otherwise change gamerunner obj to test
+splashscreen.splashscreen() #uncomment for splash screen to work, otherwise change gamerunner obj to test
 
-game = gameRunner("twoplayer", 1, 1, 1)
+#game = gameRunner("twoplayer", 1, 1, 1)
